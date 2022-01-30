cb852e6cdaf840d69e59c93d50410e20dc42c465
zouritre <timouf97400@live.fr>
2022-01-31 00:28:26 +0100

    Segue from HomeScreenVC to CombatScreenVC and passing players info

---
+++ OC_FrenchGameFactory/OC_FrenchGameFactory/Controller/CombatScreen/CombatScreenVC.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  OC_FrenchGameFactory
+//
+//  Created by Bertrand Dalleau on 30/01/2022.
+//
+
+import UIKit
+
+class CombatScreenVC: UIViewController {
+    
+    
+    //MARK: - Variables
+
+    
+    
+    
+    var gameSession = GameSession(players: [])
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //MARK: - View Did Load
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        print(gameSession)
+    }
+    
+
+}
