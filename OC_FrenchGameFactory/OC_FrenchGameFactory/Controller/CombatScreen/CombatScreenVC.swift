3bc8f5ddc8674bdf0cf22c3b5f4e2d21dcaeb618
zouritre <timouf97400@live.fr>
2022-02-01 19:50:43 +0100

    Created UI for combat screen and added IBOutlets connection to VC

--- OC_FrenchGameFactory/OC_FrenchGameFactory/Controller/CombatScreen/CombatScreenVC.swift
+++ OC_FrenchGameFactory/OC_FrenchGameFactory/Controller/CombatScreen/CombatScreenVC.swift
@@ -10,6 +10,55 @@ import UIKit
 class CombatScreenVC: UIViewController {
     
     
+    
+    
+    //MARK: - IBOutlets Player 1
+    
+    
+    
+    
+    @IBOutlet weak var player1Char1WeaponDamage: UILabel!
+    @IBOutlet weak var player1Char1HP: UILabel!
+    @IBOutlet weak var player1Char1Button: UIButton!
+    
+    @IBOutlet weak var player1Char2WeaponDamage: UILabel!
+    @IBOutlet weak var player1Char2HP: UILabel!
+    @IBOutlet weak var player1Char2Button: UIButton!
+    
+    @IBOutlet weak var player1Char3WeaponDamage: UILabel!
+    @IBOutlet weak var player1Char3HP: UILabel!
+    @IBOutlet weak var player1Char3Button: UIButton!
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //MARK: - IBOutlets Player 2
+    
+    
+    
+    
+    @IBOutlet weak var player2Char1WeaponDamage: UILabel!
+    @IBOutlet weak var player2Char1HP: UILabel!
+    
+    @IBOutlet weak var player2Char2WeaponDamage: UILabel!
+    @IBOutlet weak var player2Char2HP: UILabel!
+    
+    @IBOutlet weak var player2Char3WeaponDamage: UILabel!
+    @IBOutlet weak var player2Char3Button: UILabel!
+    
+    
+    
+    
+    
+    
+    
+    
+    
     //MARK: - Variables
 
     
