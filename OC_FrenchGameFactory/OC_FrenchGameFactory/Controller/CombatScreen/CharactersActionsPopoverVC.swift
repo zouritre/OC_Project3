ad9703508062b5b6176e7566627a4af42dcbe19f
zouritre <timouf97400@live.fr>
2022-02-02 17:18:19 +0100

    WIP - setting up VC to update combat screen UI characters stats when players chose an action

---
+++ OC_FrenchGameFactory/OC_FrenchGameFactory/Controller/CombatScreen/CharactersActionsPopoverVC.swift
@@ -0,0 +1,33 @@
+//
+//  CharactersActionsPopoverVC.swift
+//  OC_FrenchGameFactory
+//
+//  Created by Bertrand Dalleau on 02/02/2022.
+//
+
+import UIKit
+
+class CharactersActionsPopoverVC: UIViewController {
+    
+    var alliesAndFoes: [String:Player] = [:]
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print(alliesAndFoes)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- OC_FrenchGameFactory/OC_FrenchGameFactory/Controller/CombatScreen/CombatScreenVC.swift
+++ OC_FrenchGameFactory/OC_FrenchGameFactory/Controller/CombatScreen/CombatScreenVC.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+
 class CombatScreenVC: UIViewController {
     
     
@@ -49,13 +50,37 @@ class CombatScreenVC: UIViewController {
     @IBOutlet weak var player2Char2HP: UILabel!
     
     @IBOutlet weak var player2Char3WeaponDamage: UILabel!
-    @IBOutlet weak var player2Char3Button: UILabel!
+    @IBOutlet weak var player2Char3HP: UILabel!
     
     
+   
+    @IBAction func showCharActions(_ sender: UIButton) {
+        getCharacterOwningPlayer(name: sender.currentTitle!)
+        performSegue(withIdentifier: "selectCharActions", sender: self)
+    }
     
     
-    
-    
+    func getCharacterOwningPlayer(name: String){
+        var foundAlly = false
+        for player in gameSession.players {
+            if foundAlly {
+                alliesAndFoes["Foe"] = player
+            }
+            for character in player.characters {
+                if character.customName == name {
+                    alliesAndFoes["Ally"] = player
+                    foundAlly = true
+                    selectedAllyWeaponDamage = character.weapon.damage
+                    selectedAllyOwningPlayer = player
+                    break
+                }
+            }
+            if foundAlly == false {
+                alliesAndFoes["Foe"] = player
+            }
+            
+        }
+    }
     
     
     
@@ -66,6 +91,11 @@ class CombatScreenVC: UIViewController {
     
     var gameSession = GameSession(players: [])
     
+    var alliesAndFoes: [String:Player] = [:]
+    
+    var selectedAllyWeaponDamage: Int = 0
+    
+    var selectedAllyOwningPlayer: Player = Player(name: "", characters: [])
     
     
     
@@ -78,14 +108,58 @@ class CombatScreenVC: UIViewController {
     
     
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        var watchPlayer1Char1HP : Int {
+            get{
+                return 1
+            }
+            set{
+                player1Char1HP.text = "Health: \(newValue)"
+            }
+        }
         
-        
+        watchPlayer1Char1HP = 1
         print(gameSession)
+        
+        
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // MARK: - Navigation
+    
+    
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        
+        
+        
+        if segue.destination is CombatScreenVC {
+            
+            
+            
+            let vc = segue.destination as? CombatScreenVC
+                
+            vc?.alliesAndFoes = alliesAndFoes
+        
+        
+        
+        }
+        
+        
+        
+    }
+    
     
 
 }
